17aac32bfecfeb3dddb4448b28b16839315d9a45
Malee <N.Khanthamalee@gmail.com>
2025-01-23 15:27:09 +0700

    edit search field page

--- lib/data/product/source/product_firebase_service.dart
+++ lib/data/product/source/product_firebase_service.dart
@@ -64,15 +64,32 @@ class ProductFirebaseserviceImpl extends ProductFirebaseService {
   @override
   Future<Either> getProductByTitle(String title) async {
     try {
-      print("title : $title");
-      var returnedData = await FirebaseFirestore.instance
-          .collection('products')
-          .where('title', isGreaterThanOrEqualTo: title)
-          .get();
-      print(
-          "returnedData.docs.map((e) => e.data()) :${returnedData.docs.map((e) => e.data())}");
-      print(returnedData.docs.map((e) => e.data()).runtimeType);
-      return Right(returnedData.docs.map((e) => e.data()));
+      print("title : ${title.toLowerCase()}");
+
+      var data = await FirebaseFirestore.instance.collection('products').get();
+      var dataList = data.docs.map((e) => e.data()).toList();
+      List<Map<String, dynamic>> returnedData = [];
+      for (var i in dataList) {
+        if (i["title"].toLowerCase().contains(title.toLowerCase().trim())) {
+          var querydata = await FirebaseFirestore.instance
+              .collection('products')
+              .where('title', isEqualTo: i["title"])
+              .get();
+          var querydatamap = querydata.docs.map((e) => e.data());
+          print(querydata);
+          returnedData.addAll(querydatamap.toList());
+        }
+      }
+      // var returned = await FirebaseFirestore.instance
+      //     .collection('products')
+      //     .where('title', isGreaterThanOrEqualTo: title)
+      //     .get();
+      // var returnedD = returned.docs.map((e) => e.data());
+      //var ireturnedData = returnedData.iterator;
+
+      print("returnedData.docs.map((e) => e.data()) :${returnedData}");
+      print(returnedData.runtimeType);
+      return Right(returnedData);
     } catch (e) {
       return Left("Please try again later");
     }
